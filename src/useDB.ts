e818eef040da82a22cf798d5c77f9feccb3dca3a
Towski0804 <superchao0206@gmail.com>
2024-04-29 15:32:57 +0800

    Add db for dev only.

--- src/useDB.ts
+++ src/useDB.ts
@@ -1,6 +1,9 @@
 import mongoose from "mongoose";
 
+const isDev = process.env.SERVE_MODE === "dev";
+
 const dbUrl = process.env.DB_URL || "";
-const useDB = () => mongoose.connect(`${dbUrl}/eshop`);
+const useDB = () =>
+  mongoose.connect(isDev ? `${dbUrl}/eshop_dev` : `${dbUrl}/eshop`);
 
 export default useDB;
